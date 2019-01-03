2b30b6e9d6e64a242b9f2c2d11435182504c9d7c
jlopezper <jorge.loper@gmail.com>
2019-01-03 18:34:19 +0000

    update utils

--- R/utils.R
+++ R/utils.R
@@ -2,16 +2,21 @@ require(ckanr)
 
 ckanr_setup(url = "http://apidatosabiertos.laspalmasgc.es/")
 
-package_list(as = "table", limit = 200)
+package_list(as = "table", limit = 1e10)
 
 categories <- group_list(as = "table")
 
 tags <- tag_list(as = 'table')
 
-head(tags, 20)
+head(tags, 50)
 
+tag_show("6f20cde0-45e7-426e-b429-b9489e013c81")
 
-x <- package_show("ocupacion-calles")
+x <- ckanr::group_show("salud")
+
+x$packages[[1]]
+
+x <- package_show(id = "1d9867a8-4027-44b4-8f79-18deb0dee359")
 
 str(x$resources, max.level = 2)
 
