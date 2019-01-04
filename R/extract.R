37c1eb854fc88a59aeb9673f3650b43475e5b94a
jlopezper <jorge.loper@gmail.com>
2019-01-04 18:17:36 +0000

    adding extract function factory

---
+++ R/extract.R
@@ -0,0 +1,24 @@
+extract_generator <- function(feature) {
+  force(feature)
+  function(x_list) {
+    if (!feature %in% names(x_list)) {
+      return("No available")
+    }
+
+
+    feature <- x_list[[feature]]
+    feature[feature == ""] <- NA
+
+    feature
+
+  }
+
+}
+
+extract_author <- extract_generator("author")
+extract_maintainer <- extract_generator("maintainer")
+extract_id <- extract_generator("id")
+extract_name <- extract_generator("name")
+extract_notes <- extract_generator("notes")
+extract_created <- extract_generator("metadata_created")
+
--- R/utils.R
+++ R/utils.R
@@ -21,3 +21,33 @@ x <- package_show(id = "1d9867a8-4027-44b4-8f79-18deb0dee359")
 str(x$resources, max.level = 2)
 
 df <- readr::read_csv(x$resources[[1]]$url)
+
+
+#' Setting API connection
+#'
+#' @return Connection to the LPGC hometown open data website
+api_conn <- function() {
+  ckanr_setup(url = "http://apidatosabiertos.laspalmasgc.es/")
+}
+
+#' Categories of open data available on the City Council of Las Palmas de Gran Canaria.
+#'
+#' @export
+#'
+#' @examples
+#' openlpgc_categories
+lpgc_show_categories <- group_list(as = "table")
+
+
+lpgc_categories <- function(category) {
+  # Check if category is a character vector
+  if(!is.character(category)) stop("category is not a character vector")
+
+  # Check if any category is not available
+  is_cat_available <- category %in% lpgc_show_categories
+  if(any(!is_cat_available)) {
+    stop(paste("Category/ies", category[!is_cat_available] ,"is/are not available. Please check lpgc_show_categories to check the available ones.", sep = " "))
+  }
+
+
+}
