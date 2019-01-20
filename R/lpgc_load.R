31d2e0123bd95c72a58ccd8a00a6243b84699349
jlopezper <jorge.loper@gmail.com>
2019-01-20 23:32:14 +0100

    adding trycatch

--- R/lpgc_load.R
+++ R/lpgc_load.R
@@ -1,9 +1,12 @@
 lpgc_load <- function(id) {
+  if(!all(is.character(id), length(id) == 1)) stop("id must be a character vector of length 1")
   returned_df <-
     structure(
       list(
-        metadata = extract_metadata(id),
-        data = extract_data(id)
+        metadata = tryCatch(expr = extract_metadata(id),
+                            error = function(e) message("***** id not found *****")),
+        data = tryCatch(expr = extract_data(id),
+                        error = function(e) NULL)
       )
     )
   returned_df
