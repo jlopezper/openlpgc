d141181c1a7a66f82d60f06a2bd8681fe2ac5742
jlopezper <jorge.loper@gmail.com>
2019-01-20 11:03:09 +0100

    adding extract metada funcion

--- R/extract.R
+++ R/extract.R
@@ -23,6 +23,29 @@ extract_notes <- extract_generator("notes")
 extract_created <- extract_generator("metadata_created")
 
 
-extract_metadata <- function(category, func) {
+extract_all_categories <- function(category, func) {
   sapply(category, func)
 }
+
+
+extract_metadata <- function(packages) {
+  # Get metadata
+  author <- extract_all_categories(category = packages, func = extract_author)
+  maintainer <- extract_all_categories(category = packages, func = extract_maintainer)
+  id <- extract_all_categories(category = packages, func = extract_id)
+  name <- extract_all_categories(category = packages, func = extract_name)
+  notes <- extract_all_categories(category = packages, func = extract_notes)
+  created <- lubridate::as_date(lubridate::ymd_hms(extract_all_categories(category = packages, func = extract_created)))
+
+  # Create tibble with main info
+  df <-
+    tibble::tibble(author = author,
+                   maintainer = maintainer,
+                   id = id,
+                   name = name,
+                   notes = notes,
+                   created = created)
+
+  df
+}
+
