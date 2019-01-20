1c17f037a2c69e758d016bb2593157ae6512cb68
jlopezper <jorge.loper@gmail.com>
2019-01-20 23:12:18 +0100

    cleaning lpgc_load

--- R/lpgc_load.R
+++ R/lpgc_load.R
@@ -2,7 +2,7 @@ lpgc_load <- function(id) {
   returned_df <-
     structure(
       list(
-        metadata = extract_metadata(list(ckanr::package_show(id))),
+        metadata = extract_metadata(id),
         data = extract_data(id)
       )
     )
