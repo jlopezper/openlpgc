994ac0afbbd69d9ce5c0ced930f0decdac53d4cb
jlopezper <jorge.loper@gmail.com>
2019-01-27 22:46:16 +0100

    updating doc and cleaning utils.R up

--- R/lpgc_load.R
+++ R/lpgc_load.R
@@ -1,3 +1,14 @@
+#' Load the data from Las Palmas de Gran Canaria City Council.
+#'
+#' @inheritParams extract_metadata
+#' @return A list with two slots:
+#' \itemize{
+#'  \item A \code{\link[tibble]{tibble}} with metadata provided by the site.
+#'  \item If a single file, a \code{\link[tibble]{tibble}} with the data. If multiple files, a list with as many slots as files are read.
+#'  }
+#' @export
+#'
+#' @examples \dontrun{lpgc_load(id = "47ea65a4-bc0e-42e1-8bfc-8bd3508642e6")}
 lpgc_load <- function(id) {
   if(!all(is.character(id), length(id) == 1)) stop("id must be a character vector of length 1")
   returned_df <-
--- R/utils.R
+++ R/utils.R
@@ -1,28 +1,3 @@
-require(ckanr)
-
-ckanr_setup(url = "http://apidatosabiertos.laspalmasgc.es/")
-
-package_list(as = "table", limit = 1e10)
-
-categories <- group_list(as = "table")
-
-tags <- tag_list(as = 'table')
-
-head(tags, 50)
-
-tag_show("6f20cde0-45e7-426e-b429-b9489e013c81")
-
-x <- ckanr::group_show("salud")
-
-x$packages[[1]]
-
-x <- package_show(id = "1d9867a8-4027-44b4-8f79-18deb0dee359")
-
-str(x$resources, max.level = 2)
-
-df <- readr::read_csv(x$resources[[1]]$url)
-
-
 #' Setting API connection
 #'
 #' @return Connection to the LPGC hometown open data website
